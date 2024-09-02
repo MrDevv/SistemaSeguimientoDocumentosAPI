ee79b37367ed022df45b829e84148d7f22d8ea3b
Miguel Vega Perez <aries_242015@hotmail.com>
2024-09-01 19:57:04 -0500

    init

---
+++ src/main/java/com/mrdevv/SistemaSeguimientoDocumentosApiApplication.java
@@ -0,0 +1,13 @@
+package com.mrdevv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaSeguimientoDocumentosApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaSeguimientoDocumentosApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mrdevv/SistemaSeguimientoDocumentosApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mrdevv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaSeguimientoDocumentosApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
